0ea5d630ba61b3fcbb71c2783f862e3e2295770f
Anne Jones <anne1365@outlook.com>
2022-07-11 06:48:24 -0700

    install eui dependecies. Co-authored-by: Pauline3414159 <fagie37@hotmail.com> Co-authored-by: olgashi <oshiryaeva@protonmail.com> Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@
 /* eslint-disable import/extensions */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { EuiProvider } from '@elastic/eui';
 import App from './App.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <EuiProvider>
+      <App />
+    </EuiProvider>
   </React.StrictMode>,
 );
