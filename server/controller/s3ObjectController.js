d02149701fc266a6ef6b0ca4aef171b6e66ee446
Pauline3414159 <fagie37@hotmail.com>
2022-07-14 15:44:24 -0400

    restore s3object controller to orignal so this can be merged.
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>
    Co-authored-by: aelizj <anne1365@outlook.com>

--- server/controller/s3ObjectController.js
+++ server/controller/s3ObjectController.js
@@ -22,21 +22,21 @@ const getS3Objects = async(req, res, next) => {
 const rehydrateS3Object = async(req, res, next) => {
   try {
     const { objectKey } = req.body
-    // const data = await getObjectContents(objectKey)
-    // const rawLogString = await streamToString(data.Body);
-    // const logsJson = logStringToJson(rawLogString);
-    // await postToLogstash(logsJson)
+    const data = await getObjectContents(objectKey)
+    const rawLogString = await streamToString(data.Body);
+    const logsJson = logStringToJson(rawLogString);
+    await postToLogstash(logsJson)
     res.status(202).json({
       message: `Rehydrate on ${objectKey} in progress`,
     })
   } catch(err) {
     console.log("Error", err)
-    // res.status(err.$metadata.httpStatusCode).send({
-    //   fault: err.$fault, 
-    //   status: err.$metadata.httpStatusCode,
-    //   type: err.name,
-    //   message: err.message,
-    // })
+    res.status(err.$metadata.httpStatusCode).send({
+      fault: err.$fault, 
+      status: err.$metadata.httpStatusCode,
+      type: err.name,
+      message: err.message,
+    })
   }
 }
 
