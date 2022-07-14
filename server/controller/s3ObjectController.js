d1fcef24d28ee3a7eaf1a193369642d22a3949a1
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-07-14 12:46:45 -0500

    Update catch blocks to have more generic error handling

--- server/controller/s3ObjectController.js
+++ server/controller/s3ObjectController.js
@@ -5,38 +5,34 @@ const { postToLogstash } = require("../services/logstashService");
 
 const getS3Objects = async(req, res, next) => {
   try {
-    const  response  = await getBucketObjects()
+    const response  = await getBucketObjects();
     const objectKeys = response.Contents?.map(({Key}) => Key);
-    res.send({objectKeys})
-  } catch (err) {
-    console.log("Error", err.$metadata);
-    res.status(err.$metadata.httpStatusCode).send({
-      fault: err.$fault, 
-      status: err.$metadata.httpStatusCode,
-      type: err.name,
-      message: err.message,
-    })
-;  }
+    res.send({objectKeys});
+  } catch (error) {
+    console.log("Error: ", error);
+    res.status(500).send({
+      message: "Fetching object from S3 failed, see error message for more details", 
+      error
+    });
+  }
 }
 
 const rehydrateS3Object = async(req, res, next) => {
   try {
-    const { objectKey } = req.body
-    const data = await getObjectContents(objectKey)
+    const { objectKey } = req.body;
+    const data = await getObjectContents(objectKey);
     const rawLogString = await streamToString(data.Body);
     const logsJson = logStringToJson(rawLogString);
-    await postToLogstash(logsJson)
+    await postToLogstash(logsJson);
     res.status(202).json({
       message: `Rehydrate on ${objectKey} in progress`,
     })
-  } catch(err) {
-    console.log("Error", err)
-    res.status(err.$metadata.httpStatusCode).send({
-      fault: err.$fault, 
-      status: err.$metadata.httpStatusCode,
-      type: err.name,
-      message: err.message,
-    })
+  } catch(error) {
+    console.log("Error: ", error);
+    res.status(500).send({
+      message: "Log rehydration failed, see error message for more details", 
+      error
+    });
   }
 }
 
