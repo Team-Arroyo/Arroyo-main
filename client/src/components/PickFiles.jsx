541ea6c7610f1c5fe28e5e515e8e93aad47e666f
Pauline3414159 <fagie37@hotmail.com>
2022-07-15 14:38:42 -0400

    created PickFiles component.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ client/src/components/PickFiles.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import {
+  EuiButton, EuiSpacer, EuiFlexGroup, EuiFlexItem,
+} from '@elastic/eui';
+import apiClient from '../libs/apiclient';
+import Dropdown from './Dropdown';
+
+function PickFiles() {
+  const [choices, setChoices] = useState([]);
+  const [choice, setChoice] = useState('');
+
+  useEffect(() => {
+    apiClient.getKeys().then(
+      (keys) => setChoices(keys),
+    ).catch((e) => {
+      console.log(e);
+    });
+  }, []);
+
+  const handleSelection = (e) => {
+    setChoice(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (choice === 'Select A Log') {
+      console.log('Nothing was selected');
+      return;
+    }
+    apiClient.getObject(choice)
+      .then(
+        (r) => console.log(r),
+      );
+  };
+
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+        <Dropdown choices={choices} onSelection={handleSelection} />
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiButton onClick={handleClick}>Ingest Log</EuiButton>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+
+export default PickFiles;
