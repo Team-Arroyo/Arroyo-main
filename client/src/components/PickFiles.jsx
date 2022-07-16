ba8581dd8cce524ae18265c20c0e402cd6cb3acd
Pauline3414159 <fagie37@hotmail.com>
2022-07-16 10:00:10 -0400

    update utils so only selected keys are returned on clicking ingest button; pickfilese euiselectable is working as expected
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/components/PickFiles.jsx
+++ client/src/components/PickFiles.jsx
@@ -1,31 +1,41 @@
 import React, { useState, Fragment, useEffect } from 'react';
-import { EuiSelectable } from '@elastic/eui';
+import { EuiSelectable, EuiButton, EuiSpacer } from '@elastic/eui';
 import PropTypes from 'prop-types';
 import convert from '../libs/utils';
 
 function PickFiles({ choices }) {
   const starting = convert.toOptions(choices);
   const [options, setOptions] = useState([]);
+  const handleClick = () => {
+    const selected = convert.toKeys(options);
+    console.log(selected);
+  };
 
   useEffect(() => {
     setOptions(starting);
   }, [choices]);
 
   return (
-    <EuiSelectable
-      aria-label="Basic example"
-      searchable
-      options={options}
-      listProps={{ bordered: true }}
-      onChange={(newOptions) => setOptions(newOptions)}
-    >
-      {(list, search) => (
-        <Fragment key="searchable">
-          {search}
-          {list}
-        </Fragment>
-      )}
-    </EuiSelectable>
+    <>
+      <EuiSelectable
+        aria-label="Basic example"
+        searchable
+        options={options}
+        listProps={{ bordered: true }}
+        onChange={(newOptions) => setOptions(newOptions)}
+      >
+        {(list, search) => (
+          <Fragment key="searchable">
+            {search}
+            {list}
+          </Fragment>
+        )}
+      </EuiSelectable>
+      <EuiSpacer size="l" />
+      <EuiButton onClick={handleClick}>
+        Ingest Logs
+      </EuiButton>
+    </>
   );
 }
 
--- client/src/libs/utils.js
+++ client/src/libs/utils.js
@@ -2,7 +2,9 @@ const toOptions = (s3Keys) => s3Keys.map((key) => ({
   label: key,
 }));
 
-const toKeys = (options) => options.map((option) => option.label);
+const toKeys = (options) => options
+  .filter((option) => option.checked)
+  .map((option) => option.label);
 
 const convert = {
   toOptions,
