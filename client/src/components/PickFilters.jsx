d9d3f094289198ec6a570b87af9f320b7c43afcc
Pauline3414159 <fagie37@hotmail.com>
2022-07-18 15:42:22 -0400

    add validation logic and error messages for dates; refactor PickFilters to use EuiForm component.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/components/PickFilters.jsx
+++ client/src/components/PickFilters.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import React, { useState } from 'react';
 import {
-  EuiButton, EuiSpacer, EuiFlexGroup, EuiFlexItem, EuiTitle, EuiText,
+  EuiButton, EuiSpacer, EuiTitle, EuiText, EuiForm, EuiFormRow, EuiFlexGroup, EuiFlexItem,
 } from '@elastic/eui';
 import moment from 'moment';
 import PropTypes from 'prop-types';
@@ -12,17 +12,20 @@ function PickFilters({ setChoices }) {
   const [startDate, setStartDate] = useState(moment());
   const [endDate, setEndDate] = useState(moment());
 
+  const formatDate = (dateObj) => {
+    if (!dateObj) return null;
+    return moment(dateObj).format('MM-DD-YYYY');
+  };
+
+  const isValidDateRange = (!formatDate(startDate) && !formatDate(endDate))
+  || (startDate !== null && endDate !== null && startDate <= endDate);
+
   const handleStartDateChange = (date) => {
     setStartDate(date);
   };
 
   const handleEndDateChange = (date) => {
     setEndDate(date);
-  };
-
-  const formatDate = (dateObj) => {
-    if (!dateObj) return null;
-    return moment(dateObj).format('MM-DD-YYYY');
   };
 
   const handleClick = () => {
@@ -34,25 +37,49 @@ function PickFilters({ setChoices }) {
       .catch((e) => console.log(e));
   };
 
+  const getErrorMessage = () => {
+    if (!formatDate(startDate) || !formatDate(endDate)) return ['Enter start and end date'];
+    if (!isValidDateRange) return ['Start date cannot be greater than end date'];
+    return ['Error'];
+  };
+
   return (
     <div>
       <EuiTitle size="s"><h2>Filter</h2></EuiTitle>
       <EuiSpacer size="m" />
       <EuiText><p>Select a Date Range</p></EuiText>
-      <EuiSpacer size="s" />
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <DatePicker dateType="start date" dateStatus={startDate} handleChange={handleStartDateChange} />
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <DatePicker dateType="end date" dateStatus={endDate} handleChange={handleEndDateChange} />
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiButton onClick={handleClick}>
+      <EuiSpacer size="m" />
+      <EuiForm>
+        <EuiFormRow
+          isInvalid={!isValidDateRange}
+          error={getErrorMessage()}
+        >
+          <EuiFlexGroup>
+            <EuiFlexItem>
+              <DatePicker
+                dateType="start date"
+                dateStatus={startDate}
+                handleChange={handleStartDateChange}
+              />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <DatePicker
+                dateType="end date"
+                dateStatus={endDate}
+                handleChange={handleEndDateChange}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFormRow>
+        <EuiFormRow>
+          <EuiButton
+            onClick={handleClick}
+            isDisabled={!isValidDateRange}
+          >
             Search S3
           </EuiButton>
-        </EuiFlexItem>
-      </EuiFlexGroup>
+        </EuiFormRow>
+      </EuiForm>
       <EuiSpacer size="xl" />
     </div>
   );
