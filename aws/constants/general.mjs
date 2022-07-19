00c720d78ef23b58f462c5044863391472af7fa7
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-07-19 17:13:16 -0500

    add constants Co-authored-by: aelizj <anne1365@outlook.com> Co-authored-by: Pauline3414159 <fagie37@hotmail.com> Co-authored-by: olgashi <oshiryaeva@protonmail.com> Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ aws/constants/general.mjs
@@ -0,0 +1,11 @@
+const CONTENT_TYPE_APPLICATION_ZIP = "application/zip";
+const ACL_PRIVATE = "private";
+const OPEN = "open";
+const ERROR = "error";
+
+export {
+  CONTENT_TYPE_APPLICATION_ZIP,
+  ACL_PRIVATE,
+  OPEN,
+  ERROR
+}
