cd4d00af974aac4cbcf63e718af20ba4e61c78eb
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-07-19 17:13:43 -0500

    add sqs functions Co-authored-by: aelizj <anne1365@outlook.com> Co-authored-by: Pauline3414159 <fagie37@hotmail.com> Co-authored-by: olgashi <oshiryaeva@protonmail.com> Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ aws/libs/sqs/createQueue.mjs
@@ -0,0 +1,15 @@
+import { CreateQueueCommand, GetQueueAttributesCommand } from "@aws-sdk/client-sqs";
+import sqsClient from "../clients/sqsClient.mjs";
+
+const createQueue = async (queueParams) => {
+  try {
+    const createQueueCommand = new CreateQueueCommand(queueParams);
+    const createQueueResponse = await sqsClient.send(createQueueCommand);
+
+    return { QueueUrl: createQueueResponse.QueueUrl };
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+export default createQueue;
---
+++ aws/libs/sqs/getQueueAttributes.mjs
@@ -0,0 +1,25 @@
+import { GetQueueAttributesCommand } from "@aws-sdk/client-sqs";
+import sqsClient from "../clients/sqsClient.mjs";
+
+const getQueueAttributes = async ({ attributesArray, queueURL }) => {
+  try {
+    const getQueueAttributesCommand = new GetQueueAttributesCommand({
+      AttributeNames: attributesArray, 
+      QueueUrl: queueURL
+    });
+
+    const getQueueAttributesResponse = await sqsClient.send(getQueueAttributesCommand);
+    const attributesObject = getQueueAttributesResponse.Attributes;
+    
+    const resultObj = {};
+    for (const attribute in attributesObject) {
+      resultObj[attribute] = attributesObject[attribute];
+    }
+
+    return resultObj;
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+export default getQueueAttributes;
