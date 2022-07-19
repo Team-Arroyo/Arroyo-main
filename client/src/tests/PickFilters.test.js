e1333cba5293a7fa1a1099d5260a066cc7b1451e
Pauline3414159 <fagie37@hotmail.com>
2022-07-19 14:23:49 -0400

    found way to get the clear date buttons, finished invalid date input makes a warning test.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/tests/PickFilters.test.js
+++ client/src/tests/PickFilters.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event';
 import {render, screen, fireEvent} from '@testing-library/react';
 import PickFilters from "../components/PickFilters";
 
@@ -10,11 +11,26 @@ describe('Pick Filters', () => {
     }
   );
   test('renders date picker', () => {
-    
     expect(screen.getByLabelText(/start date/i)).toBeInTheDocument();
   });
 
-  test('does it re-render each time', () => {
-    screen.debug()
+  test('renders searchS3 button', () => {
+    expect(screen.getByText(/Search S3/)).toBeInTheDocument();
+  });
+
+  test('invalid date input makes a warning', async () => {
+    const clearDateButtons = screen.getAllByRole('button', {name: /Clear input/});
+    await userEvent.click(clearDateButtons[0])
+    expect(
+      screen.getByText(/Enter start and end date/i)
+    ).toBeInTheDocument();
+  });
+
+  xtest('invalid date input disables searchS3 button', () => {
+
+  })
+
+  xtest('clicking searchS3 button calls fakeSetChoices', () => {
+
   })
 })
