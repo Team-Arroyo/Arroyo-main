2546753d8537103137e5e01ad5ca9789ae1d0a64
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-07-19 21:05:23 -0500

    added create role function Co-authored-by: aelizj <anne1365@outlook.com> Co-authored-by: Pauline3414159 <fagie37@hotmail.com> Co-authored-by: olgashi <oshiryaeva@protonmail.com> Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ aws/libs/iam/createRole.mjs
@@ -0,0 +1,20 @@
+import iamClient from "../clients/iamClient.mjs";
+import { CreateRoleCommand } from "@aws-sdk/client-iam";
+import { LAMBDA_ASSUME_ROLE_POLICY } from "../../constants/awsPolicies.mjs";
+
+const createRole = async (roleName) => {
+  const roleParams = {
+      AssumeRolePolicyDocument: LAMBDA_ASSUME_ROLE_POLICY,
+      Path: "/",
+      RoleName: roleName
+  };
+    try {
+      const data = await iamClient.send(new CreateRoleCommand(roleParams));
+      return { roleARN: data.Role.Arn};
+    } catch (err) {
+      console.log("Error", err);
+    }
+};
+
+export default createRole;
+
