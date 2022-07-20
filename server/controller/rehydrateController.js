a23782b2bdddd560fb3ca154ad262d8589cfe259
Chip Dean <xxchipdeanxx@gmail.com>
2022-07-20 13:38:27 -0700

    reverted initializeRehydrateJob to reflect whats on main branch

--- server/controller/rehydrateController.js
+++ server/controller/rehydrateController.js
@@ -8,28 +8,17 @@ const isTotalFailure = (batch) => {
 const initializeRehydrateJob = (req, res) => {
   console.log("Rehydrating tasks in progress...")
   const keyError = req.keyError;
-  const queryError = req.queryError;
 
   const objectKeys = req.objectKeys;
-  const sqlExpression = req.sqlExpression;
 
   if(keyError) {
     console.log("keyError detected");
     console.log("Rehydrating tasks stopped...");
     res.status(400).json(keyError);
     return;
-  } else if (queryError) {
-    console.log("queryError detected");
-    console.log("Rehydrating tasks stopped...");
-    res.status(400).json(queryError);
-    return;
   }
   
-  const promises  = objectKeys.map(objectKey => {
-    return sqlExpression ? 
-      rehydrateQueriedS3Object(objectKey, sqlExpression) 
-      :rehydrateFullS3Object(objectKey)
-  });
+  const promises  = objectKeys.map(objectKey => rehydrateFullS3Object(objectKey))
 
   Promise.allSettled(promises).then(resultArray => {
     console.log("All promises settled");
@@ -40,7 +29,6 @@ const initializeRehydrateJob = (req, res) => {
 
     res.status(isTotalFailure(batchStatus) ? 400 : 200).json({batchStatus})
   });
-  // res.status(500).json({message: 'in dev'});
 }
 
 const initializeQueryRehydrate = async(req, res) => {
