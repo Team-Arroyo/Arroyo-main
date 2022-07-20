23d1aa290524335438c904aba647bf16b7273a28
Pauline3414159 <fagie37@hotmail.com>
2022-07-20 08:18:39 -0400

    add before each block; added comments for tests that address the Selectable part of PickFiles.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/tests/PickFiles.test.js
+++ client/src/tests/PickFiles.test.js
@@ -5,25 +5,40 @@ import { render, screen } from '@testing-library/react';
 import PickFiles from '../components/PickFiles'
 
 describe('Pick Files', () => {
-  test('pick files renders', () => {
+  beforeEach(() => {
     render(<PickFiles />)
+    }
+  );
+  test('pick files renders', () => {
     expect(screen.getByText('Select')).toBeInTheDocument();
   });
   test('empty choice list when no choices are passed down', () => {
-    render(<PickFiles />)
     const para = screen.getAllByText((content, element) => element.tagName.toLowerCase() === 'p' && element.innerHTML === 'No options available') 
     expect(para.length).toBeGreaterThan(1);
   });
-  xtest('choices render when choices are passed down', () => {
+  xtest('ingest button is disabled when no choice is selected', () => {
 
   });
+  xtest('clear all button is rendered', () => {
+    
+  });
+  xtest('select all button is rendered', () => {
+    
+  });
+  // below tests don't work; likely due to elements of interest being out of scope
+  // as the elements of interest are returned by an annoymouse function lines 41-46
+  xtest('choices render when choices are passed down', () => {
+    const choices = ['log file one', 'test2', 'three']
+    // screen can't grab any of the choices even though the choices are passed down correctly
+    // and are even correctly mapped into the options array
+    render(<PickFiles choices={choices}/>)
+  });
+  // can't write the rest of these without being able to grab the elements of interest
   xtest('you can select a choice', () => {
 
   });
   xtest('you can select all choices', () => {
 
   });
-  xtest('ingest button is disabled when no choice is selected', () => {
-
-  });
+  
 })
