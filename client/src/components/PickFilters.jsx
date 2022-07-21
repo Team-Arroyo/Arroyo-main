1a7dceb83d823f2c22b26b6cc7c7b2603bdb696a
Pauline3414159 <fagie37@hotmail.com>
2022-07-21 16:09:22 -0400

    add query terms to state.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/components/PickFilters.jsx
+++ client/src/components/PickFilters.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable no-console */
 /* eslint-disable */
 
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   EuiButton,
   EuiSpacer,
@@ -11,33 +12,16 @@ import {
   EuiFormRow,
   EuiFlexGroup,
   EuiFlexItem,
-  EuiFieldText,
   EuiAccordion,
-  EuiPanel,
-  EuiButtonIcon,
 } from '@elastic/eui';
-import { useSelector, useDispatch } from 'react-redux';
-import { icon as searchIcon } from '@elastic/eui/es/components/icon/assets/search';
-import { icon as plusIcon } from '@elastic/eui/es/components/icon/assets/plus';
-// import { icon as trashIcon } from '@elastic/eui/es/components/icon/assets/trash';
 import DateRange from './DateRange';
+import QueryTerms from './QueryTerms';
 import { getKeysAndSetChoices } from '../features/choicesSlice';
 
 function PickFilters() {
   const dispatch = useDispatch();
-  const [column, setColumn] = useState('');
-  const [columnValue, setColumnValue] = useState('');
-  const [queries, setQueries] = useState([]);
   const startDate = useSelector((state) => state.dateRange.start)
   const endDate = useSelector((state) => state.dateRange.end)
-  const handleChangeColumn = (c) => setColumn(c.target.value);
-  const handleChangeColumnValue = (cv) => setColumnValue(cv.target.value);
-  const handleAddQueryClick = () => {
-    setQueries([...queries, { column: columnValue }]);
-    console.log(queries);
-    setColumn('');
-    setColumnValue('');
-  };
 
   const handleClick = () => {
     console.log('from button', startDate)
@@ -57,42 +41,7 @@ function PickFilters() {
 
         <EuiSpacer size="xl" />
         <EuiAccordion id="query-accordion" buttonContent="Add Search Query">
-          <EuiPanel color="primary">
-            <EuiFlexGroup style={{ maxWidth: 600 }} gutterSize="l" alignItems="flexEnd" justifyContent="flexEnd">
-              <EuiFlexItem>
-                <EuiFormRow label="Log Attribute" component="form">
-                  <EuiFieldText
-                    placeholder="ex. HTTP Method"
-                    value={column}
-                    onChange={handleChangeColumn}
-                    icon={searchIcon}
-                  />
-                </EuiFormRow>
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiFormRow label="Attribute Value" component="form">
-                  <EuiFieldText
-                    placeholder="ex. GET"
-                    value={columnValue}
-                    onChange={handleChangeColumnValue}
-                    icon={searchIcon}
-                  />
-                </EuiFormRow>
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiFormRow>
-                  <EuiButtonIcon
-                    iconType={plusIcon}
-                    size="m"
-                    display="base"
-                    onClick={handleAddQueryClick}
-                    aria-label="add query search term"
-                    aria-labelledby="add query search term"
-                  />
-                </EuiFormRow>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiPanel>
+          <QueryTerms />
         </EuiAccordion>
         <EuiSpacer size="xl" />
         <EuiFormRow>
@@ -117,6 +66,5 @@ function PickFilters() {
     </div>
   );
 }
-
 
 export default PickFilters;
