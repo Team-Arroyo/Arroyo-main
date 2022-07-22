7ea1a7c4afb26434d79c85bde343b91c1fd960de
Pauline3414159 <fagie37@hotmail.com>
2022-07-22 08:34:11 -0400

    date range must be valid before Select Log Files button is enabled.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/components/PickFiltersButtonGroup.jsx
+++ client/src/components/PickFiltersButtonGroup.jsx
@@ -8,6 +8,7 @@ import {
 } from '@elastic/eui';
 import { useSelector, useDispatch } from 'react-redux';
 import { getKeysAndSetChoices } from '../features/choicesSlice';
+import { isValidDateRange } from '../libs/utils';
 
 function PickFiltersButtonGroup() {
   const dispatch = useDispatch();
@@ -23,6 +24,7 @@ function PickFiltersButtonGroup() {
         <EuiFlexItem>
           <EuiButton
             onClick={handleClick}
+            isDisabled={!isValidDateRange()}
           >
             Select Log Files
           </EuiButton>
