1b79a9cfd49f22e5b745cb5f150517d9158e3fa5
Chip Dean <xxchipdeanxx@gmail.com>
2022-07-22 12:22:55 -0700

    added BatchSize param to createEventSourceMapping

--- server/aws/lambda/createEventSourceMapping.mjs
+++ server/aws/lambda/createEventSourceMapping.mjs
@@ -5,13 +5,14 @@ const createEventSourceMapping = async ({ functionName, eventSourceArn }) => {
   try {
     const eventSourceMappingCommand = new CreateEventSourceMappingCommand({
       FunctionName: functionName,
-      EventSourceArn: eventSourceArn
+      EventSourceArn: eventSourceArn,
+      BatchSize: 1
     });
     const eventSourceMappingCommandResponse = await lambdaClient.send(eventSourceMappingCommand);
     return eventSourceMappingCommandResponse.UUID;
   } catch (error) {
     console.log(error);
   }
-}
+};
 
 export default createEventSourceMapping;
