35fe61c6e17afcfb9cb86a179fcc18d8fd3d3da3
Pauline3414159 <fagie37@hotmail.com>
2022-07-25 15:44:34 -0400

    add credentials to fix sqsClient.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- server/aws/clients/sqsClient.mjs
+++ server/aws/clients/sqsClient.mjs
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const sqsClient = new SQSClient({ 
-  region: process.env.AWS_REGION
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+  }
 });
 export default sqsClient;
