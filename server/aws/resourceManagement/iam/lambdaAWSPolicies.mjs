b6acb0d9fc19d1e4f0c850b0155c8cb0ef2d99bd
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-07-25 16:00:43 -0500

    move policy consts into resourceManagement Co-authored-by: aelizj <anne1365@outlook.com> Co-authored-by: Pauline3414159 <fagie37@hotmail.com> Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ server/aws/resourceManagement/iam/lambdaAWSPolicies.mjs
@@ -0,0 +1,27 @@
+const LAMBDA_ASSUME_ROLE_POLICY = `{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+          "Service": [
+            "lambda.amazonaws.com"
+          ]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}`;
+
+const AWSXRayDaemonWriteAccessARN = 'arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess';
+const AmazonS3ReadOnlyAccessARN = 'arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess';
+const AWSLambdaSQSQueueExecutionRoleARN = 'arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole';
+const AWSLambdaBasicExecutionRoleARN = 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole';
+
+export {
+  LAMBDA_ASSUME_ROLE_POLICY,
+  AWSXRayDaemonWriteAccessARN,
+  AmazonS3ReadOnlyAccessARN,
+  AWSLambdaSQSQueueExecutionRoleARN,
+  AWSLambdaBasicExecutionRoleARN
+}
