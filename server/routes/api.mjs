30c6f03615f4f5d3e66d1c1ece3c53d2094e4275
Chip Dean <xxchipdeanxx@gmail.com>
2022-07-26 14:52:43 -0700

    removed depreciated POST /api/s3object/rehydrate route and logic

--- server/routes/api.mjs
+++ server/routes/api.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getS3Objects, rehydrateS3Object } from '../controller/s3ObjectController.mjs';
+import { getS3Objects } from '../controller/s3ObjectController.mjs';
 import { initializeRehydrateJob, initializeQueryRehydrate }  from '../controller/rehydrateController.mjs';
 
 import validateObjectKeys from '../middleware/validateObjectKeys.mjs';
@@ -10,9 +10,6 @@ import validateBodyDateParams from '../middleware/validateBodyDateParams.mjs';
 const router = express.Router();
 
 router.get('/s3objects', validateUrlDateParams, getS3Objects);
-
-//will be removed once front end switches to new api
-router.post('/s3object/rehydrate', rehydrateS3Object);
 
 router.post('/s3objects', validateObjectKeys, initializeRehydrateJob);
 
