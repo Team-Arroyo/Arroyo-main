cf271ae15e676989d6afa47906751d8cbe967ff2
Chip Dean <xxchipdeanxx@gmail.com>
2022-07-26 15:11:59 -0700

    added sseClient and backend_clients directory
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: Pauline3414159 <fagie37@hotmail.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>

--- server/controller/sseController.mjs
+++ server/controller/sseController.mjs
@@ -1,4 +1,16 @@
+import sseClient from '../backend_clients/sseClient.mjs';
 
 export const establishSseConnection = (req, res) => {
-  res.json({message: 'endpoint good'});
+  console.log('Establishing SSE connection...');
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+
+  res.writeHead(200, headers);
+  res.write('data: hello from server');
+  res.write('\n\n');
+  
+  sseClient.stream = res;
 };
