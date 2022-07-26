78489ea3af0b43b579ebc040e280bb6d370e79b8
Pauline3414159 <fagie37@hotmail.com>
2022-07-26 10:17:58 -0400

    filled suite of rendering tests for by date tab
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/tests/ByDateTab.test.js
+++ client/src/tests/ByDateTab.test.js
@@ -3,25 +3,32 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../app/store.js';
 import ByDateTab from '../components/ByDateTab';
 
 describe('by dates compnents are rendering', () => {
   beforeEach(() => {
-    render(<ByDateTab />);
+    render(
+      <Provider store={store}>
+        <ByDateTab />
+      </Provider>,
+    );
   });
-  xtest('select log files button renders', () => {
-
+  test('select log files button renders', () => {
+    expect(screen.getByRole('button', { name: /Select Log/i })).toBeInTheDocument();
   });
-  xtest('calendar inputs render', () => {
-
+  test('calendar inputs render', () => {
+    expect(screen.getByLabelText(/start date/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/end date/i)).toBeInTheDocument();
   });
-  xtest('select all files button renders', () => {
-
+  test('select all files button renders', () => {
+    expect(screen.getByRole('button', { name: /Select All/i })).toBeInTheDocument();
   });
-  xtest('clear all selections button renders', () => {
-
+  test('clear all selections button renders', () => {
+    expect(screen.getByRole('button', { name: /Clear All/i })).toBeInTheDocument();
   });
-  xtest('inglest button renders', () => {
-
+  test('inglest button renders', () => {
+    expect(screen.getByRole('button', { name: /Ingest/i })).toBeInTheDocument();
   });
 });
