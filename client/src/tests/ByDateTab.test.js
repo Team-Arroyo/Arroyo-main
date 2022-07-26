8741de566989b662a021b6aeadb8aa6c56d2090c
Pauline3414159 <fagie37@hotmail.com>
2022-07-26 09:57:11 -0400

    add spike of tests for by date tab
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

---
+++ client/src/tests/ByDateTab.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
+import ByDateTab from '../components/ByDateTab';
+
+describe('by dates compnents are rendering', () => {
+  beforeEach(() => {
+    render(<ByDateTab />);
+  });
+  xtest('select log files button renders', () => {
+
+  });
+  xtest('calendar inputs render', () => {
+
+  });
+  xtest('select all files button renders', () => {
+
+  });
+  xtest('clear all selections button renders', () => {
+
+  });
+  xtest('inglest button renders', () => {
+
+  });
+});
