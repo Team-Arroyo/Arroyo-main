59e84d20ba97eea11cad709486e8e17a8810d735
Pauline3414159 <fagie37@hotmail.com>
2022-07-27 11:55:59 -0400

    add useEffect in wrapper to create event source listener and dispatch to toast slice.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/components/Wrapper.jsx
+++ client/src/components/Wrapper.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { EuiTabbedContent, EuiText, EuiSpacer } from '@elastic/eui';
+import { STATUS_URL } from '../constants/ApiRoutes.js';
+import { addToast } from '../features/toastSlice.js';
 import ByDateTab from './ByDateTab';
 import ByQueryTab from './ByQueryTab';
 
 function Wrapper() {
+  const dispatch = useDispatch();
   const tabs = [
     {
       id: 'bydate-tab',
@@ -16,6 +20,15 @@ function Wrapper() {
       content: (<ByQueryTab />),
     },
   ];
+
+  useEffect(() => {
+    const source = new EventSource(STATUS_URL);
+    source.onmessage = (e) => {
+      const event = JSON.parse(e.data);
+      console.log('the event', event);
+      dispatch(addToast(event));
+    };
+  }, []);
 
   return (
     <>
--- client/src/constants/ApiRoutes.js
+++ client/src/constants/ApiRoutes.js
@@ -2,3 +2,4 @@ export const HOST = 'http://localhost:5001';
 export const GET_KEYS_URL = `${HOST}/api/s3objects`;
 export const HYDRATE_LOG_URL = `${HOST}/api/s3object/rehydrate`;
 export const POST_QUERY_INGEST = `${HOST}/api/query-ingest`;
+export const STATUS_URL = `${HOST}/api/statuses`;
