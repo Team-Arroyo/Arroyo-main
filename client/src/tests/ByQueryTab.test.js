ae2f1c5acee2e92677baf9cb7314f13e2d11cbd8
Pauline3414159 <fagie37@hotmail.com>
2022-07-27 15:00:13 -0400

    tests working.
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>
    Co-authored-by: donald-p-redding <donald.p.redding@gmail.com>

--- client/src/tests/ByQueryTab.test.js
+++ client/src/tests/ByQueryTab.test.js
@@ -7,6 +7,7 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../app/store.js';
 import ByQueryTab from '../components/ByQueryTab.jsx';
+import exp from 'constants';
 
 describe('by query compnents are rendering', () => {
   beforeEach(() => {
@@ -24,12 +25,51 @@ describe('by query compnents are rendering', () => {
     expect(screen.getByLabelText(/Log Attribute/i)).toBeInTheDocument();
     expect(screen.getByLabelText(/Attribute Value/i)).toBeInTheDocument();
   });
-  /* TODO: jest throws error of can't use import statement outside a module for 
-  importing icons from elasticSearch */
-  xtest('plus button renders', () => {
+  test('plus button renders', () => {
     expect(screen.getByLabelText(/add query search term/i)).toBeInTheDocument();
   });
-  xtest('inglest button renders', () => {
+  test('inglest button renders', () => {
     expect(screen.getByRole('button', { name: /Ingest Matching/i })).toBeInTheDocument();
+  });
+});
+
+describe('performing a happy path', () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <ByQueryTab />
+      </Provider>,
+    );
+  });
+  test('ingest matching button is disabled', () => {
+    expect(screen.getByRole('button', { name: /Ingest Matching/i })).toBeDisabled();
+  });
+  test('can enter query terms', async () => {
+    const logAttributes = screen.getByLabelText(/Log Attribute/i);
+    const logAttributesValues = screen.getByLabelText(/Attribute Value/i);
+    await userEvent.type(logAttributes, 'request_method');
+    await userEvent.type(logAttributesValues, 'GET');
+    expect(logAttributes).toHaveValue('request_method');
+    expect(logAttributesValues).toHaveValue('GET');
+  });
+  test('entered query terms render', async () => {
+    const logAttributes = screen.getByLabelText(/Log Attribute/i);
+    const logAttributesValues = screen.getByLabelText(/Attribute Value/i);
+    const addButton = screen.getByLabelText(/add query search term/i);
+    await userEvent.type(logAttributes, 'request_method');
+    await userEvent.type(logAttributesValues, 'GET');
+    await userEvent.click(addButton);
+    expect(screen.getByText('request_method:GET')).toBeInTheDocument();
+  });
+  test('can remove query terms by clicking on it', async () => {
+    const logAttributes = screen.getByLabelText(/Log Attribute/i);
+    const logAttributesValues = screen.getByLabelText(/Attribute Value/i);
+    const addButton = screen.getByLabelText(/add query search term/i);
+    await userEvent.type(logAttributes, 'request_method');
+    await userEvent.type(logAttributesValues, 'GET');
+    await userEvent.click(addButton);
+    const pill = screen.getByLabelText(/Click to remove Query Term/);
+    await userEvent.click(pill);
+    expect(screen.queryByText('request_method:GET')).not.toBeInTheDocument();
   });
 });
