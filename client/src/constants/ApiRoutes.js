925878bbb8b26c1ec67e8f821d3dc674ba3ceaa7
Chip Dean <xxchipdeanxx@gmail.com>
2022-07-29 11:42:26 -0700

    updated front end code to reach arroyo-server
    Co-authored-by: aelizj <anne1365@outlook.com>
    Co-authored-by: Pauline3414159 <fagie37@hotmail.com>
    Co-authored-by: olgashi <oshiryaeva@protonmail.com>

--- client/src/constants/ApiRoutes.js
+++ client/src/constants/ApiRoutes.js
@@ -1,5 +1,5 @@
-export const HOST = 'http://arroyo-server:5001';
-export const GET_KEYS_URL = `${HOST}/api/s3objects`;
-export const HYDRATE_LOG_URL = `${HOST}/api/s3object/rehydrate`;
-export const POST_QUERY_INGEST = `${HOST}/api/query-ingest`;
-export const STATUS_URL = `${HOST}/api/statuses`;
+// export const HOST = 'http://host.docker.internal:5001';
+export const GET_KEYS_URL = '/api/s3objects';
+export const HYDRATE_LOG_URL = '/api/s3object/rehydrate';
+export const POST_QUERY_INGEST = '/api/query-ingest';
+export const STATUS_URL = '/api/statuses';
--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use('/api', router);
 
